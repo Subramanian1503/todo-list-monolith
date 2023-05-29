f02b0c102671c376a2f7fa1a030921534ddefb7d
subramanian1503 <roopan1503@gmail.com>
2023-05-29 17:46:47 +0530

    Fix the home page ejs formatting issue

--- routers/index.js
+++ routers/index.js
@@ -6,11 +6,11 @@ const todoRouter = express.Router();
 console.log(`The router for home created successfully`);
 
 // Configure required URLS to route to the home controller
-const homeController = require('../controllers/home_controller');
+const homeController = require("../controllers/home_controller");
 todoRouter.get("/start", homeController.home);
 
 // Configuring required URLS to route to the todo_task_controller
-const todo_task_router = require('./todo_task');
+const todo_task_router = require("./todo_task");
 todoRouter.use("/todo", todo_task_router);
 
 // Export the created router to be configured in the main class
